581b175ffa856db5b84221a886410f23b69a20d0
Ivan Sousa <ivanjsousa@gmail.com>
2023-02-02 19:53:20 -0300

    visualização do relatorio com o pygal

--- book_curso_intensivo/python_repos.py
+++ book_curso_intensivo/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 
 # Faz uma chamada de API e armazena a resposta
@@ -13,14 +15,17 @@ print("Total repositories:", response_dict['total_count'])
 
 # Explora informações sobre os repositórios
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
-print("\nSelected information about each repository:")
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('\nName:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Created:', repo_dict['created_at'])
-    print('Updated:', repo_dict['updated_at'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Cria a visualização
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Projetos Python com mais estrelas no GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_respo.svg')
