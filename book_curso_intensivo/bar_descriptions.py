e7372d2e80008d2d3fc6da81001fc329b089ba3c
Ivan Sousa <ivanjsousa@gmail.com>
2023-02-06 21:03:43 -0300

    corrigindo o digitação

--- book_curso_intensivo/bar_descriptions.py
+++ book_curso_intensivo/bar_descriptions.py
@@ -2,7 +2,7 @@ import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 my_style = LS('#333366', base_style=LCS)
-chart = paygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
 
 chart.title = 'Python Projects'
 chart.x_labels = ['hattpie', 'django', 'flask']
